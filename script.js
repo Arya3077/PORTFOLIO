2432342a4987f09068ab1b0985bce91372384c51
Arya shibu <arya.shibu.dhanya1200@gmail.com>
2025-02-06 00:08:40 +0530

    commit

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const menu = document.getElementById("menu")
 const sidebar= document.getElementsByClassName("sidebar")[0];
 const x= document.getElementById("X");
+const d= document.getElementById("d");
+const nav= document.getElementsByTagName("nav");
+const body= document.body;
+
 
 menu.addEventListener("click",()=>{
     sidebar.style.display="flex";
@@ -11,3 +15,14 @@ menu.addEventListener("click",()=>{
 
 x.addEventListener("click",()=>{
     sidebar.style.display="none";})
+
+d.addEventListener("click",()=>{
+     if (body.classList.contains("dark-mode")) {
+        // If dark mode is active, switch back to light mode
+        body.classList.remove("dark-mode");
+    } else {
+        // If dark mode is not active, apply dark mode
+        body.classList.add("dark-mode");
+    }
+})
+   
